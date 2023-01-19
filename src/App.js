4cab20c64543437b29bf234d475c4cda83911247
heejaelee <hj.lee2@stickint.kr>
2023-01-19 13:36:42 +0900

    useTab

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import useInput from './hooks/useInput'
+import useTab from './hooks/useTab'
 import './App.css'
 
+const content = [
+  {
+    tab: 'section 01',
+    content: 'content 01'
+  },
+  {
+    tab: 'section 02',
+    content: 'content 02'
+  }
+]
+
 function App() {
+  // useInput
   const maxLength = (value) => value.length <= 10
   const name = useInput('Mr.', maxLength)
 
+  // useTab
+  const {currentIndex, setCurrentIndex} = useTab(0, content)
+
   return (
-    <div className="App">
-      <input type="text" placeholder="write your name" {...name}/>
+    <div className="app">
+      <section>
+        <h3>useInput</h3>
+        <input type="text" placeholder="write your name" {...name}/>
+      </section>
+
+
+      <section>
+        <h3>useTab</h3>
+        {
+          content.map((section, index) => (
+            <button onClick={() => setCurrentIndex(index)}>{section.tab}</button>
+          ))
+        }
+        <div>{currentIndex.content}</div>
+      </section>
     </div>
   )
 }
---
+++ src/hooks/useTab.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+
+const useTab = (initialTab, allTabs) => {
+  const [currentIndex, setCurrentIndex] = useState(initialTab)
+
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return
+  }
+
+  return {
+    currentIndex: allTabs[currentIndex],
+    setCurrentIndex
+  }
+}
+
+export default useTab
